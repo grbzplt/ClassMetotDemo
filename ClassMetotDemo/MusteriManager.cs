f85bbfc74c611da3b17c5788908b31f88a5f83e9
Grbz Plt <grbzplt@hotmail.com>
2021-01-17 16:38:12 +0300

    Ders3 Ödev 3

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("{0} {1} ", musteri.Ad, musteri.Soyad + " eklendi...");
+            
+        }
+
+        public void MusteriListele(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("{0} {1} {2}", musteri.Ad, musteri.Soyad,musteri.HesapNo);
+            }
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("{0} {1} ", musteri.Ad, musteri.Soyad + " silindi...");
+        }
+
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,41 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            Musteri musteri2 = new Musteri();
+            Musteri musteri3 = new Musteri();
+                    
+            musteri1.Ad = "Engin";
+            musteri1.Soyad = "Demiroğ";
+            musteri1.HesapNo = 68456;
+
+            musteri2.Ad = "Kerem";
+            musteri2.Soyad = "Varış";
+            musteri2.HesapNo = 74521;
+
+            musteri3.Ad = "Berkay";
+            musteri3.Soyad = "Bilgin";
+            musteri3.HesapNo = 90123;
+                  
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3};
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            Console.WriteLine("---------------Müşteri Listesi------------------");
+            musteriManager.MusteriListele(musteriler);
+            Console.WriteLine("-------------------------------------------------");
+
+            Musteri musteri4 = new Musteri();
+            musteri4.Ad = "Murat";
+            musteri4.Soyad = "Kurtboğan";
+            musteri4.HesapNo = 104560;
+
+            musteriManager.MusteriEkle(musteri4);
+
+            Console.WriteLine("---------------------------------");
+
+            musteriManager.MusteriSil(musteri2);
+
         }
     }
 }
